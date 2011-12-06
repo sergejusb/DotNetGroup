39555dc5f1deff011d8a503cbe2799880e4ed0e3
Sergejus <seriog@yahoo.com>
2011-12-06 23:17:21 +0200

    Added UrlContentProcessor

---
+++ Tests/Services/Web/UrlContentProcessorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Services.Web;
+
+namespace Tests.Services.Web
+{
+    [TestFixture]
+    public class UrlContentProcessorTests
+    {
+        [Test]
+        public void Given_Tweet_With_The_Sorten_Url_Content_After_Processing_Href_Tag_Is_Successfully_Replaced_With_The_Original()
+        {
+            var contentBeforeProcessing = "ITishnikai #7 jau online! Svečiuose Romualdas (@<a class=\" \" href=\"http://twitter.com/rstonkus\">rstonkus</a>) Stonkus <em><a href=\"http://search.twitter.com/search?q=%23ltnet\" title=\"#ltnet\" class=\" \">#ltnet</a></em> <a href=\"http://t.co/lzi51BTM\">http://t.co/lzi51BTM</a>";
+            var contentAfterProcessing = "ITishnikai #7 jau online! Svečiuose Romualdas (@<a class=\" \" href=\"http://twitter.com/rstonkus\">rstonkus</a>) Stonkus <em><a href=\"http://search.twitter.com/search?q=%23ltnet\" title=\"#ltnet\" class=\" \">#ltnet</a></em> <a href=\"http://t.co/lzi51BTM\">http://sergejus.blogas.lt/itishnikai-7-jau-online-1586.html</a>";
+            var processor = new UrlContentProcessor();
+
+            var result = processor.Process(contentBeforeProcessing);
+
+            Assert.AreEqual(contentAfterProcessing, result);
+        }
+    }
+}
