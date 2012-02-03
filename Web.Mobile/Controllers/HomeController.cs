140d2e6e408a44894a0d3839df9d53c0cf0199d0
Giedrius Banaitis <giedriusbanaitis@gmail.com>
2012-02-03 17:33:20 +0200

    Added mobile website. Reconfigured websites to use IIS.

---
+++ Web.Mobile/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using AutoMapper;
+using Web.Mobile.Models.ViewModels;
+using Web.Mobile.Services;
+
+namespace Web.Mobile.Controllers
+{
+    using System;
+
+    using MongoDB.Bson;
+
+    using Web.Mobile.Models;
+
+    using global::Services.Model;
+
+    public partial class HomeController : Controller
+    {
+        private readonly IStreamService streamService;
+
+        public HomeController() : this(new StreamService())
+        {            
+        }
+
+        public HomeController(IStreamService streamService)
+        {
+            this.streamService = streamService;
+        }
+
+        public virtual ActionResult Index(StreamFilter filter)
+        {
+            var enumerable = Mapper.Map<IEnumerable<ItemCompactView>>(streamService.GetItems(filter));
+            return View(enumerable);
+        }
+
+        public virtual ActionResult Item(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return HttpNotFound();
+            }
+
+            ObjectId objectId;
+            try
+            {
+                objectId = new ObjectId(id);
+            }
+            catch 
+            {
+                return HttpNotFound();
+            }
+
+            var item = streamService.GetItem(objectId);
+
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            var itemView = Mapper.Map<ItemView>(item);
+            return View(itemView);
+        }
+    }
+}
---
+++ Web.Mobile/Services/StreamService.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Services.Model;
+using Web.Mobile.Models;
+
+namespace Web.Mobile.Services
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Web.Script.Serialization;
+
+    using MongoDB.Bson;
+
+    public interface IStreamService
+    {
+        IEnumerable<Item> GetItems(StreamFilter filter);
+        Item GetItem(ObjectId objectId);
+    }
+
+    public class StreamService : IStreamService
+    {
+        public IEnumerable<Item> GetItems(StreamFilter filter)
+        {
+            // TODO replace with actual implementation, that gives combined sream by using filter
+            using (var webClient = new WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
+                var jsonSerializer = new JavaScriptSerializer();
+                var rssItems = jsonSerializer.Deserialize<IEnumerable<Item>>(webClient.DownloadString("http://api.dotnetgroup.dev/rss/json"));
+                var twitterItems = jsonSerializer.Deserialize<IEnumerable<Item>>(webClient.DownloadString("http://api.dotnetgroup.dev/twitter/json?from=2010-01-01"));
+                var items = rssItems.Union(twitterItems).OrderBy(x => Guid.NewGuid()).AsQueryable();
+
+                if (filter == null)
+                {
+                    return items;
+                }
+
+                if (filter.DateFrom.HasValue)
+                {
+                    items = items.Where(x => x.Published >= filter.DateFrom.Value.Date);
+                }
+
+                if (filter.DateTo.HasValue)
+                {
+                    items = items.Where(x => x.Published <= filter.DateFrom.Value.Date.AddDays(1));
+                }
+
+                if (filter.ItemType.HasValue)
+                {
+                    items = items.Where(x => x.ItemType == filter.ItemType);
+                }
+
+                if (filter.PageIndex.HasValue)
+                {
+                    items = items.Skip(filter.PageIndex.Value * filter.PageSize).Take(filter.PageSize);
+                }
+
+                return items;
+            }
+        }
+
+        public Item GetItem(ObjectId objectId)
+        {
+            // todo: replace with actual implementation
+            return this.GetItems(null).FirstOrDefault(x => x.Id == objectId);
+        }
+    }
+}
