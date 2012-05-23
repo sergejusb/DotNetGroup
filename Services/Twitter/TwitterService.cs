220bb8e53218e984522934e427a55dc76738a466
Sergejus <seriog@yahoo.com>
2012-05-23 20:08:00 +0300

    Updated libraries

--- Services/Twitter/TwitterService.cs
+++ Services/Twitter/TwitterService.cs
@@ -23,39 +23,50 @@
         {
             try
             {
-                var context = new TwitterContext();
-
-                // for some reasons only Date part of DateTime is accepted as an argument for Twitter's since query
-                var date = fromDate == DateTime.MinValue ? DateTime.UtcNow.AddMonths(-1).Date : fromDate.Date;
-                
-                var result = (from search in context.Search
-                              where search.Type == SearchType.Search
-                              && search.Query == query
-                              && search.Since == date
-                              && search.WithRetweets == false
-                              && search.PageSize == Count
-                              select search).First().Entries;
-
-                // due to the aforementioned limitation need to perform additional filtering
-                result = result.Where(e => e.Published > fromDate).ToList();
-
-                return result.Select(e => new Item
+                using (var context = new TwitterContext())
                 {
-                    Url = e.Alternate,
-                    Published = e.Published,
-                    AuthorImage = e.Image,
-                    AuthorName = e.Author.Name,
-                    AuthorUri = e.Author.URI,
-                    Title = string.Empty,
-                    Content = e.Content,
-                    Tags = ExtractTags(e.Content),
-                    ItemType = ItemType.Twitter
-                }).ToList();
+                    // for some reasons only Date part of DateTime is accepted as an argument for Twitter's since query
+                    var date = fromDate == DateTime.MinValue ? DateTime.UtcNow.AddMonths(-1).Date : fromDate.Date;
+
+                    var result = (from search in context.Search
+                                  where search.Type == SearchType.Search
+                                  && search.Query == query
+                                  && search.Since == date
+                                  && search.WithRetweets == false
+                                  && search.PageSize == Count
+                                  select search).First().Results;
+
+                    // due to the aforementioned limitation need to perform additional filtering
+                    result = result.Where(t => t.CreatedAt.LocalDateTime > fromDate).ToList();
+
+                    return result.Select(t => new Item
+                    {
+                        Url = GetTweetUrl(t.FromUser, t.ID),
+                        Published = t.CreatedAt.LocalDateTime,
+                        AuthorImage = t.ProfileImageUrl,
+                        AuthorName = t.FromUserName,
+                        AuthorUri = GetUserUrl(t.FromUser),
+                        Title = string.Empty,
+                        Content = t.Text,
+                        Tags = ExtractTags(t.Text),
+                        ItemType = ItemType.Twitter
+                    }).ToList();
+                }
             }
             catch
             {
                 return new List<Item>();
             }
+        }
+
+        private static string GetTweetUrl(string userName, object tweetId)
+        {
+            return string.Format("https://twitter.com/{0}/status/{1}", userName, tweetId);
+        }
+
+        private static string GetUserUrl(string userName)
+        {
+            return string.Format("https://twitter.com/{0}", userName);
         }
 
         private static string[] ExtractTags(string content)
