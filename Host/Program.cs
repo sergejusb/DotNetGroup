2e76162ede5971c9a7163e7ca26f8e666297b2f1
Sergejus <seriog@yahoo.com>
2012-06-02 19:19:47 +0300

    Added dirty error handling for the host. Need to change to normal Windows Service class

--- Host/Program.cs
+++ Host/Program.cs
@@ -11,27 +11,36 @@
     {
         public static void Main(string[] args)
         {
-            var reprocessArgs = new[] { "-r", "-reprocess" };
-            var refreshPeriod = TimeSpan.FromSeconds(int.Parse(ConfigurationManager.AppSettings["sys.refreshPeriodInSec"] ?? "60"));
-            var connectionString = ConfigurationManager.AppSettings["db.connection"];
-            var database = ConfigurationManager.AppSettings["db.database"];
-            var streamPersister = new StreamPersister(connectionString, database);
+            try
+            {
+                var reprocessArgs = new[] { "-r", "-reprocess" };
+                var refreshPeriod =
+                    TimeSpan.FromSeconds(int.Parse(ConfigurationManager.AppSettings["sys.refreshPeriodInSec"] ?? "60"));
+                var connectionString = ConfigurationManager.AppSettings["db.connection"];
+                var database = ConfigurationManager.AppSettings["db.database"];
+                var streamPersister = new StreamPersister(connectionString, database);
 
-            if (args != null && args.Length == 1 && reprocessArgs.Contains(args[0].ToLower()))
-            {
-                streamPersister.Reprocess();
-            }
-            else
-            {
-                while (true)
+                if (args != null && args.Length == 1 && reprocessArgs.Contains(args[0].ToLower()))
                 {
-                    Console.WriteLine("Running...");
-
-                    streamPersister.PersistLatest();
-
-                    Console.WriteLine("Sleeping for " + refreshPeriod.TotalSeconds + " sec");
-                    Thread.Sleep(refreshPeriod);
+                    streamPersister.Reprocess();
                 }
+                else
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Running...");
+
+                        streamPersister.PersistLatest();
+
+                        Console.WriteLine("Sleeping for " + refreshPeriod.TotalSeconds + " sec");
+                        Thread.Sleep(refreshPeriod);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
             }
         }
     }
