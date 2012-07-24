01832770cf0952aad21935c98dacc62b2f9598d8
Sergejus <seriog@yahoo.com>
2012-07-24 23:02:49 +0300

    Added InternalVisibleTo attribute for testability

--- shared/SharedAssemblyInfo.cs
+++ shared/SharedAssemblyInfo.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("DotNetGroup")]
 [assembly: AssemblyCopyright("DotNetGroup, 2012")]
 
+[assembly: InternalsVisibleTo("Tests")]
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("1.0.*")]
