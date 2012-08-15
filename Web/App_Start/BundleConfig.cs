1b12460249138c7aa7b92726a5cc903d161fe2bc
Sergejus <seriog@yahoo.com>
2012-08-15 17:28:16 +0300

    Fixed bundling issue

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -8,18 +8,21 @@
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            var css = new Bundle("~/Public/css", new LessTransform(), new CssMinify());
+            var css = new Bundle("~/public/static/css", new CssMinify());
             css.Include("~/Public/css/bootstrap.css")
-               .Include("~/Public/css/bootstrap-responsive.css")
-               .Include("~/Public/css/fonts.css")
-               .Include("~/Public/css/styles.less");
+                .Include("~/Public/css/bootstrap-responsive.css")
+                .Include("~/Public/css/fonts.css");
             bundles.Add(css);
 
-            var js = new Bundle("~/Public/js", new JsMinify());
+            var less = new Bundle("~/public/static/less", new LessTransform(), new CssMinify());
+            less.Include("~/Public/css/styles.less");
+            bundles.Add(less);
+
+            var js = new Bundle("~/public/static/js", new JsMinify());
             js.Include("~/Public/js/lib/jquery-1*")
               .Include("~/Public/js/lib/bootstrap.js")
               .Include("~/Public/js/lib/spin.js")
-              .Include("~/Public/js/lib/jquery-spin.js")
+              .Include("~/Public/js/lib/jquery.spin.js")
               .Include("~/Public/js/scripts.js");
             bundles.Add(js);
         }
