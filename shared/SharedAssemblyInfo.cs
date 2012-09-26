19e7e914d4869c9f8a439523fb95e1d1321b7b82
Sergejus <seriog@yahoo.com>
2012-09-26 21:00:17 +0300

    Recreated VS solution and projects

--- shared/SharedAssemblyInfo.cs
+++ shared/SharedAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("DotNetGroup")]
 [assembly: AssemblyCopyright("DotNetGroup, 2012")]
 
-[assembly: InternalsVisibleTo("Tests")]
+[assembly: InternalsVisibleTo("DotNetGroup.Tests")]
 [assembly: ComVisible(false)]
 
 [assembly: AssemblyVersion("1.0.*")]
