66506aec9f98d7d076cf3afb0c7790a68351eebb
Sergejus <seriog@yahoo.com>
2013-01-06 20:25:14 +0200

    added static front-end page

---
+++ Web/Controllers/PageController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DotNetGroup.Web.Controllers
+{
+    public class PageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
